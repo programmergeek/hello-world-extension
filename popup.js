1fafba68091d686b6e7150ed36fcef5ab2db978d
Will <programergeek@gmail.com>
2022-03-12 23:18:10 +0200

    implement logic

--- popup.js
+++ popup.js
@@ -1,5 +1,20 @@
 let changeColour = document.getElementById("changeColour")
 
+changeColour.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setPageBackgroundColor
+    })
+})
+
+function setPageBackgroundColor() {
+    chrome.storage.sync.get("color", ({color}) => {
+        document.body.style.backgroundColor = color
+    })
+}
+
 chrome.storage.sync.get("color", ({color}) => {
     changeColour.style.background = color
 })
